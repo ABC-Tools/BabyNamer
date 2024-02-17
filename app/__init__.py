43ace4245b34c90dca9c77047e23d2510368de4a
tansan78 <tansan78@gmail.com>
2024-02-17 10:09:08 -0800

    change logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,31 @@
 import logging
-from logging import handlers
 import sys
 import os
 
 context = os.environ.get('CONTEXT', 'flask')
 
 if context != 'worker' and 'unittest' not in sys.modules.keys():
+    log_format = logging.Formatter(
+        '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+    )
+
     if os.environ.get('ENV', 'PROD') == 'DEV':
-        logging.basicConfig(format='%(asctime)s %(message)s',
-                            filename='/var/log/flask.log',
-                            level=logging.DEBUG)
+        logging.basicConfig(level=logging.DEBUG)
     else:
-        logging.basicConfig(format='%(asctime)s %(message)s',
-                            filename='/var/log/flask.log',
-                            level=logging.INFO)
+        logging.basicConfig(level=logging.INFO)
 
-    root_logger = logging.getLogger()
-    werkzeug_logger = logging.getLogger("werkzeug")  # grabs underlying WSGI logger
+    # root_logger = logging.getLogger()
+    # werkzeug_logger = logging.getLogger("werkzeug")  # grabs underlying WSGI logger
 
-    # handler = logging.FileHandler('/var/log/flask.log')  # creates handler for the log file
-    handler = logging.handlers.TimedRotatingFileHandler('/var/log/flask.log', when='D')  # creates handler for the log file
-    handler.setFormatter("%(asctime)s %(levelname)s %(message)s")
+    file_handler = logging.FileHandler('/var/log/flask.log')  # creates handler for the log file
+    # file_handler = logging.handlers.TimedRotatingFileHandler('/var/log/flask.log', when='D')
+    file_handler.setFormatter(log_format)
+    # werkzeug_logger.addHandler(file_handler)  # adds handler to the werkzeug WSGI logger
+    # root_logger.addHandler(file_handler)
 
-    werkzeug_logger.addHandler(handler)  # adds handler to the werkzeug WSGI logger
-    werkzeug_logger.addHandler(logging.StreamHandler(sys.stdout))
-    root_logger.addHandler(handler)
-    root_logger.addHandler(logging.StreamHandler(sys.stdout))
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(log_format)
+    # werkzeug_logger.addHandler(stdout_handler)
+    # root_logger.addHandler(stdout_handler)
+
+    logging.root.handlers = [stdout_handler, file_handler]
--- worker/__init__.py
+++ worker/__init__.py
@@ -1,12 +1,23 @@
 import logging
 import os
+import sys
 
 context = os.environ.get('CONTEXT', 'flask')
 
 if context == 'worker':
+    log_format = logging.Formatter(
+        '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+    )
+
     if os.environ.get('ENV', 'PROD') == 'DEV':
-        logging.basicConfig(format='%(asctime)s %(message)s',
-                            level=logging.DEBUG)
+        logging.basicConfig(level=logging.DEBUG)
     else:
-        logging.basicConfig(format='%(asctime)s %(message)s',
-                            level=logging.INFO)
+        logging.basicConfig(level=logging.INFO)
+
+    # root_logger = logging.getLogger()
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(log_format)
+    # root_logger.addHandler(stdout_handler)
+
+    logging.root.handlers = [stdout_handler]
